a7a3ed6b2f2b9c824071395e8c5e0ed48363c4e7
Colm Ryan <colm@colmryan.org>
2014-07-23 14:50:53 -0400

    Start of julia bindings to libaps2

---
+++ src/julia/APS2.jl
@@ -0,0 +1,26 @@
+type APS2
+	serial::ASCIIString
+end
+
+push!(DL_LOAD_PATH, "/home/cryan/Programming/Repos/libaps2/build/")
+
+ccall((:init, "libaps2"), Cint, ())
+
+function connect(aps::APS2, serial)
+	ccall((:connect_APS, "libaps2"), Cint, (Ptr{Cchar},), serial)
+	aps.serial = serial
+end
+
+disconnect(aps::APS2) = ccall((:disconnect_APS, "libaps2"), Cint, (Ptr{Cchar},), aps.serial)
+
+function read_memory(aps::APS2, addr, numWords)
+	buf = Array(Uint32, numWords)
+	ccall((:read_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, buf, numWords)
+	buf
+end
+
+function write_memory(aps::APS2, addr, data::Vector{Uint32})
+	ccall((:write_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, data, length(data))
+end
+
+
