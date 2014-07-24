115f0156a06d78b45a7e9a6e0eb5228908044355
Colm Ryan <colm@colmryan.org>
2014-07-24 14:50:01 -0400

    Update julia script for hacking around on sequences.

--- src/julia/createLLmem.jl
+++ src/julia/createLLmem.jl
@@ -1,7 +1,9 @@
 module APSInstructions
 
-export APSInstr, instruction, wf_entry, flow_entry
-# , LLcall, LLreturn, LLgoto, LLrepeat, LLload, waitTrig!, write_file, testSeq, testSeq2, simpleSeq
+using HDF5
+
+export APSInstr, wf_entry, flow_call, flow_return, flow_repeat, load_repeat, sync, wait_trig
+# write_file, write_hdf5_file
 
 import Base.show
 
@@ -18,7 +20,7 @@ APSInstr(data::Vector{Uint16}, name::Symbol) = APSInstr(data, name, emptySymbol)
 
 function show(io::IO, e::APSInstr)
 	for b in e.data
-		@printf(io, "%04x", b)
+		print(io, hex(b, 4))
 	end
 end
 
@@ -30,91 +32,109 @@ function write_file(filename, seq)
 	end
 end
 
+function write_hdf5_file(filename, seq, wfA, wfB)
+
+	if isfile(filename)
+		rm(filename)
+	end
+
+	#Stack the instruction data
+	instrs = Uint64[]
+	for s in seq
+		r = uint64(0)
+		for ct in 1:4
+			r += uint64(s.data[ct]) << 16*(4-ct) 
+		end
+		push!(instrs, r)
+	end
+
+	#write it to the h5 file
+	h5write(filename, "chan_1/instructions", instrs)
+
+	#write the waveform data
+	h5write(filename, "chan_1/waveforms", wfA);
+	h5write(filename, "chan_2/waveforms", wfB);
+end
+
 # instruction encodings
 WFM = 0x0000
 TRIG = 0x0001
 WAIT_TRIG = 0x0002
 LOAD_REPEAT = 0x0003
 DEC_REPEAT = 0x0004
-GOTO = 0x0005
-CALL = 0x0006
-RET = 0x0007
-SYNC = 0x0008
+CMP = 0x0005
+GOTO = 0x0006
+CALL = 0x0007
+RET = 0x0008
+SYNC = 0x0009
 
 #The top bits of the 48 bit instruction payload set the sync or wait for trigger in the playback engines
 WAIT_TRIG_INSTR_WORD = 1 << 14;
 SYNC_INSTR_WORD = 1 << 15;
+PREFETCH_INSTR_WORD = 3 << 14;
 
 # CMP op encodings
-EQUAL = 0x0004 | 0x0000
-NOTEQUAL = 0x0004 | 0x0001
-GREATERTHAN = 0x0004 | 0x0002
-LESSTHAN = 0x0004 | 0x0003
+EQUAL = 0x0000
+NOTEQUAL = 0x0001
+GREATERTHAN = 0x0002
+LESSTHAN = 0x0003
 
+#Waveform payload slv is 47 downto 0
+# Bits 47-46 are op-code (play wf / wait for trig / wait for sync )
+# Bit 45 is the TAPair flag
+# Bits 44 downto 24 is count
+# Bits 23 downto 0 is addr
 function wf_entry(addr, count; TAPair=false, writeData=true, label=emptySymbol)
-	instr = APSInstr(Uint16[WFM << 12 | (writeData ? 1 : 0) << 8, (TAPair ? 1 : 0) << 13  | count >> 8 , (count & 0xff) << 8 | addr >> 16, addr & 0xff], label)
-	instr
+	@assert (addr & 0xffffff) == addr "Oops! We can only handle 24 bit addresses"
+	@assert (count & 0xfffff) == count "Oops! We can only handle 20 bit counts"
+
+	APSInstr(Uint16[WFM << 12 | (writeData ? 1 : 0) << 8, (TAPair ? 1 : 0) << 13  | count >> 8 , (count & 0x00ff) << 8 | addr >> 16, addr & 0xffff], label)
 end
+
+wf_prefetch(addr) = APSInstr(Uint16[WFM << 12 | 1 << 8, PREFETCH_INSTR_WORD, addr >> 16, addr & 0xffff])
 
 function trig_entry(target, state, count, transitionWord; writeData=true, label=emptySymbol)
-	instr = APSInstr(Uint16[TRIG << 12 | (writeData ? 1 : 0) << 8 | (target & 0x3), 0, (transitionWord & 0xf) << 1 | (state & 0x1), count & 0xff], label)
-	instr
+	@assert (count & 0xffffffff) == count "Oops! We can only handle 32 bit trigger counts"
+
+	APSInstr(Uint16[TRIG << 12 | (writeData ? 1 : 0) << 8 | (target & 0x3) << 10, (transitionWord & 0xf) << 1 | (state & 0x1), count >> 16, count & 0xffff], label)
 end
 
-function flow_entry(cmd, cmp=0, mask=0, addr=0; label=emptySymbol)
-	instr = APSInstr(Uint16[(cmd << 12) | (cmp << 8) | (mask & 0xff), 0, addr >> 16, addr & 0xff], label)
+flow_entry(cmd, jumpAddr::Integer, payload; label=emptySymbol) =
+	APSInstr(Uint16[(cmd << 12), payload, jumpAddr >> 16, jumpAddr & 0xffff], label)
 
-	if (cmd == WAIT_TRIG)
-		instr.data[1] |= 1 << 8;
-		instr.data[2] = WAIT_TRIG_INSTR_WORD;
-	end
-	if (cmd == SYNC)
-		instr.data[1] |= 1 << 8;
-		instr.data[2] = SYNC_INSTR_WORD;
-	end
-	instr
+flow_entry(cmd, jumpAddr::Symbol, payload; label=emptySymbol) =
+	APSInstr(Uint16[(cmd << 12), payload, 0x0000, 0x0000], label, jumpAddr)
+
+function wait_trig(; label=emptySymbol)
+	instr = flow_entry(WAIT_TRIG, 0, WAIT_TRIG_INSTR_WORD; label=label)
+	instr.data[1] |= 1 << 8; # set broadcast bit
+	return instr
 end
 
+function sync(; label=emptySymbol)
+	instr = flow_entry(SYNC, 0, SYNC_INSTR_WORD; label=label)
+	instr.data[1] |= 1 << 8; # set broadcast bit
+	return instr
+end
 
+function load_repeat(loadValue; label=emptySymbol)
+	@assert loadValue < 2^16 "Oops! We can only handle 16 bit repeat counts."
+	flow_entry(LOAD_REPEAT, loadValue, 0; label=label)
+end
 
-# # function LLdata(addr, count, repeat, trigA=0, TAPair=0; label=emptySymbol)
-# # 	entry = LLentry(Uint16[TAPair << 14 | addr >> 16, addr & 0xff, count, trigA, 0x0000, repeat])
-# # 	entry.name = label
-# # 	entry
-# # end
+flow_cmp(cmpOp, mask; label=emptySymbol) = APSInstr(Uint16[CMP << 12, 0, 0, ((cmpOp & 0x03) << 8) | (mask & 0xff)], label)
 
-# # function LLcommand(cmd, mask=0x0000, addr::Integer=0x000000, count=0x0000; label=emptySymbol)
-# # 	entry = LLentry(Uint16[cmd << 8 | mask, addr >> 16, addr & 0xff, count, 0x0000, 0x0000])
-# # 	entry.name = label
-# # 	entry
-# # end
+flow_repeat(target; label=emptySymbol) = flow_entry(DEC_REPEAT, target, 0; label=label)
+flow_goto(target; label=emptySymbol) = flow_entry(GOTO, target, 0; label=label)
+flow_call(target; label=emptySymbol) = flow_entry(CALL, target, 0; label=label)
+flow_return(; label=emptySymbol) = flow_entry(RET, 0, 0; label=label)
 
-# # function LLcommand(cmd, mask, addr::Symbol, count=0; label=emptySymbol)
-# # 	entry = LLentry(Uint16[cmd << 8 | mask, 0, 0, count, 0x0000, 0x0000])
-# # 	entry.name = label
-# # 	entry.target = addr
-# # 	entry
-# # end
-
-# # LLcall(addr; label=emptySymbol) = LLcommand(CALL, 0, addr; label=label)
-# # LLcall(mask, addr; label=emptySymbol) = LLcommand(CALL | EQUAL << 3, mask, addr; label=label)
-# # LLreturn(; label=emptySymbol) = LLcommand(RET; label=label)
-# # LLreturn(mask; label=emptySymbol) = LLcommand(RET | EQUAL << 3, mask; label=label)
-# # LLgoto(addr; label=emptySymbol) = LLcommand(GOTO, 0, addr; label=label)
-# # LLrepeat(addr; label=emptySymbol) = LLcommand(REPEAT, 0, addr; label=label)
-# # LLload(count; label=emptySymbol) = LLcommand(LOAD, 0, 0, count; label=label)
-
-# # function waitTrig!(entry)
-# # 	entry.data[1] |= 1 << 15
-# # 	entry
-# # end
-
-# # updates the address in a command instruction
-# function updateAddr!(entry, addr)
-# 	entry.data[3] = addr >> 16
-# 	entry.data[4] = addr & 0xff
-# 	entry
-# end
+# updates the address in a command instruction
+function updateAddr!(entry, addr)
+	entry.data[3] = addr >> 16
+	entry.data[4] = addr & 0xffff
+	entry
+end
 
 function resolve_symbols!(seq)
 	labeledEntries = filter((x) -> x[2].name != emptySymbol, collect(enumerate(seq)))
@@ -134,124 +154,130 @@ end
 
 # # test sequences
 
+# function simpleSeq()
+# 	seq = APSInstr[]
+# 	push!(seq, wf_entry(0, 63; label=:A))
+# 	push!(seq, wf_entry(0, 7; label=:B))
+# 	push!(seq, wf_entry(8, 7; label=:C))
+# 	push!(seq, wf_entry(16, 7))
+# 	push!(seq, wf_entry(24, 7))
+# 	push!(seq, wf_entry(32, 7))
+# 	push!(seq, flow_entry(GOTO, 0, 0, 0))
+# 	resolve_symbols!(seq)
+# end
 function simpleSeq()
 	seq = APSInstr[]
-	push!(seq, wf_entry(0, 63; label=:A))
-	push!(seq, wf_entry(0, 7; label=:B))
-	push!(seq, wf_entry(8, 7; label=:C))
-	push!(seq, wf_entry(16, 7))
-	push!(seq, wf_entry(24, 7))
-	push!(seq, wf_entry(32, 7))
-	push!(seq, flow_entry(GOTO, 0, 0, 0))
+	push!(seq, sync())
+	push!(seq, load_repeat(1))
+	push!(seq, wf_entry(0x1A, 3; label=:A))
+	push!(seq, flow_cmp(EQUAL, 0x01))
+	push!(seq, flow_call(:C))
+	push!(seq, wf_entry(0x2A, 3))
+	push!(seq, flow_repeat(:A))
+	push!(seq, load_repeat(2))
+	push!(seq, wf_entry(0x3A, 3; label=:B))
+	push!(seq, wf_entry(0x4A, 4; label=:C))
+	push!(seq, wf_entry(0x5A, 4))
+	push!(seq, wf_entry(0x6A, 3))
+	push!(seq, flow_repeat(:B))
+	push!(seq, wf_entry(0x00, 3; label=:D))
+	push!(seq, flow_cmp(EQUAL, 0x02))
+	push!(seq, flow_return())
+	push!(seq, flow_goto(:D))
 	resolve_symbols!(seq)
 end
 
 function ramsey()
 	seq = APSInstr[]
 	for delay in 10:10:100
-		push!(seq, flow_entry(SYNC))
-		push!(seq, flow_entry(WAIT_TRIG))
+		push!(seq, sync())
+		push!(seq, wait_trig())
 		push!(seq, wf_entry(0, 9, TAPair=true, writeData=false))
-		push!(seq, trig_entry(0, 0, 8, 0x3))
-		push!(seq, wf_entry(8, 7))
-		push!(seq, trig_entry(0, 1, 7, 0xC))
+		push!(seq, trig_entry(0, 0, 8, 0xC))
+		push!(seq, wf_entry(13, 11))
+		push!(seq, trig_entry(0, 1, 11, 0x3))
 		push!(seq, wf_entry(0, delay, TAPair=true))
-		push!(seq, trig_entry(0, 0, delay, 0x3))
-		push!(seq, wf_entry(16, 7))
-		push!(seq, trig_entry(0, 1, 7, 0xC))
+		push!(seq, trig_entry(0, 0, delay, 0xC))
+		push!(seq, wf_entry(1, 11))
+		push!(seq, trig_entry(0, 1, 11, 0x3))
+		push!(seq, trig_entry(0, 0, 1, 0x0))
 	end
-	push!(seq, flow_entry(GOTO))
+	push!(seq, flow_goto(0x00))
 	resolve_symbols!(seq)
 end
 
-# function testSeqA()
-# 	seq = LLentry[]
-# 	push!(seq, LLdata(0, 7, 0, 0))		# addr, cout, repeat, trigA                         # 0
-# 	push!(seq, LLdata(8, 7, 1, 0))                                                          # 1
-# 	push!(seq, LLdata(0, 7, 0, 2))                                                          # 2
-# 	seq
-# end
+function cpmg()
+	seq = APSInstr[]
+	for (ct,rep) in enumerate(2.^[1:5])
+		push!(seq, sync())
+		push!(seq, wait_trig())
 
-# function testSeqB()
-# 	seq = testSeq()
-# 	push!(seq, LLload(1))				          # load global repeat count = 1             # 3
-# 	push!(seq, LLdata(0, 7, 0, 0; label=:A))	  # wf - addr, cout, repeat, trigA           # 4 - A
-# 	push!(seq, LLcall(0x01, :C))                  # mask, addr                               # 5 
-# 	push!(seq, LLdata(8, 7, 0, 0))                # wf - addr, cout, repeat, trigA           # 6
-# 	push!(seq, LLrepeat(:A))			          # repeat - ll - addr                       # 7
-# 	push!(seq, LLload(2))				          # load global repeat 2                     # 8
-# 	push!(seq, LLdata(0, 7, 0, 4; label=:B))      # wf - addr, cout, repeat, trigA              # 9  - B
-# 	push!(seq, LLdata(8, 7, 0, 2; label=:C))      # wf - addr, cout, repeat, trigA              # 10 - C
-# 	push!(seq, LLdata(0, 7, 0, 0))                # wf - addr, cout, repeat, trigA              # 11
-# 	push!(seq, LLdata(8, 7, 0, 0))                # wf - addr, cout, repeat, trigA              # 12
-# 	push!(seq, LLrepeat(:B))                      # repeat - ll - addr                       # 13
-# 	push!(seq, LLdata(0x00, 7, 0, 0; label=:D))   # wf - addr, cout, repeat, trigA           # 14 - D
-# 	push!(seq, LLreturn(0x02))                    # return - mask                            # 15
-# 	push!(seq, LLgoto(:D))                        # jump to addr  (should be the repeat 9)   # 16
-# 	resolve_symbols!(seq)
-# end
+		#First 90
+		push!(seq, wf_entry(0, 9, TAPair=true, writeData=false))
+		push!(seq, trig_entry(0, 0, 8, 0xC))
+		push!(seq, wf_entry(1, 11))
+		push!(seq, trig_entry(0, 1, 11, 0x3))
 
-# function testCacheJump()
-# 	seq = LLentry[]
-# 	push!(seq, LLdata(1, 7, 0; label=:A))	# 0
-# 	push!(seq, LLdata(2, 7, 0 ))
-# 	push!(seq, LLdata(3, 7, 0 ))	
-# 	push!(seq, LLdata(4, 7, 0 ))
-# 	push!(seq, LLdata(5, 7, 0 ))	
-# 	push!(seq, LLgoto(:C))      			# 5 - jump C                 
-# 	push!(seq, LLdata(6, 7, 0; label=:B))	# 6
-# 	push!(seq, LLdata(7, 7, 0))
-# 	push!(seq, LLdata(8, 7, 0))	
-# 	push!(seq, LLdata(9, 7, 0))
-# 	push!(seq, LLdata(10, 7, 0))
-# 	push!(seq, LLgoto(:A))                  # 11 - jump A   
-# 	push!(seq, LLdata(11, 7, 0; label=:C))	# 12
-# 	push!(seq, LLdata(12, 7, 0))
-# 	push!(seq, LLdata(13, 7, 0;))	
-# 	push!(seq, LLdata(14, 7, 0;))
-# 	push!(seq, LLdata(16, 7, 0;))	
-# 	push!(seq, LLgoto(:B))   				# 17
-# 	resolve_symbols!(seq)
-# end
+		#CPMG block repeats
+		push!(seq, load_repeat(rep-1))
 
-# function testCacheJump2()
-#         seq = LLentry[]
-#         push!(seq, LLdata(1, 7, 0; label=:A))	# 0
-#         push!(seq, LLgoto(:C))      			# 1 - jump C
-#         push!(seq, LLdata(3, 7, 0; label=:B))   # 2
-#         push!(seq, LLgoto(:D))      			# 3 - jump D
-#         push!(seq, LLdata(5, 7, 0; label=:C))	# 4
-#         push!(seq, LLgoto(:B))      			# 5 - jump B
-#         push!(seq, LLdata(7, 7, 0; label=:D))   # 6
-#         push!(seq, LLgoto(:A))      			# 7 - jump C
-#         resolve_symbols!(seq)
-# end
+		push!(seq, wf_entry(0, 0xA, TAPair=true; label=symbol("C$ct")))
+		push!(seq, trig_entry(0, 0, 9, 0xC))
+		push!(seq, wf_entry(25, 11))
+		push!(seq, trig_entry(0, 1, 11, 0x3))
+		push!(seq, wf_entry(0, 0x14, TAPair=true))
+		push!(seq, trig_entry(0, 0, 0x13, 0xC))
+		push!(seq, wf_entry(25, 11))
+		push!(seq, trig_entry(0, 1, 11, 0x3))
+		push!(seq, wf_entry(0, 0xA, TAPair=true))
+		push!(seq, trig_entry(0, 0, 9, 0x0))
+		push!(seq, flow_repeat(symbol("C$ct")))
 
-# function testCacheJump3()
-#         seq = LLentry[]
-#         push!(seq, LLdata(1, 7, 0))	# 0
-#         push!(seq, LLgoto(:A))      	# 1 - jump A
-#         push!(seq, LLdata(3, 7, 0))	# 2
-#         push!(seq, LLdata(4, 7, 0))	# 3
-#         push!(seq, LLdata(5, 7, 0))	# 4
-#         push!(seq, LLdata(6, 7, 0))	# 5
-#         push!(seq, LLdata(7, 7, 0))	# 6
-#         push!(seq, LLdata(8, 7, 0))	# 7
-#         push!(seq, LLdata(9, 7, 0))	# 8
-#         push!(seq, LLdata(10, 7,0; label=:A))	# 9
-#         push!(seq, LLgoto(:C))      	# 10 - jump C
-#         push!(seq, LLdata(12, 7, 0; label=:B))   # 11
-#         push!(seq, LLgoto(:D))      	# 12 - jump D
-#         push!(seq, LLdata(14, 7, 0; label=:C))	# 13
-#         push!(seq, LLgoto(:B))      	# 14
-#         push!(seq, LLdata(16, 7, 0; label=:D))   # 15
-#         push!(seq, LLgoto(:A))  	# 16
-#         resolve_symbols!(seq)
-# end
+		#Final 90
+		push!(seq, wf_entry(1, 11))
+		push!(seq, trig_entry(0, 1, 11, 0x3))
+	end
+	push!(seq, flow_goto(0x00))
+	resolve_symbols!(seq)
+end
+
+function rabi_amp()
+	seq = APSInstr[]
+	for ct in 1:100
+		push!(seq, sync())
+		push!(seq, wait_trig())
+		push!(seq, wf_entry(0, 9, TAPair=true, writeData=false))
+		push!(seq, trig_entry(0, 0, 8, 0xC))
+		push!(seq, wf_entry((ct-1)*256 + 1, 255))
+		push!(seq, trig_entry(0, 1, 255, 0x3))
+		push!(seq, wf_entry(0, 9, TAPair=true))
+		push!(seq, trig_entry(0, 0, 9, 0x0))
+	end
+	push!(seq, flow_goto(0x00))
+end	
+
+function test_prefetch()
+	seq = APSInstr[]
+	offset = 0
+	for offset = 0:2^15:(3*2^15)
+		push!(seq, wf_prefetch(offset))
+		for ct in 1:100
+			push!(seq, sync())
+			push!(seq, wait_trig())
+			push!(seq, wf_entry(offset+0, 9, TAPair=true, writeData=false))
+			push!(seq, trig_entry(0, 0, 8, 0xC))
+			push!(seq, wf_entry(offset+ (ct-1)*256 + 1, 255))
+			push!(seq, trig_entry(0, 1, 255, 0x3))
+			push!(seq, wf_entry(offset+0, 9, TAPair=true))
+			push!(seq, trig_entry(0, 0, 9, 0x0))
+		end
+	end
+	push!(seq, flow_goto(0x00))
+end
 
 function writeTestFiles()
-        seq = simpleSeq();
-        write_file("simpleSeq.dat", seq)
+	seq = simpleSeq();
+	write_file("simpleSeq.dat", seq)
 end
 
 end
