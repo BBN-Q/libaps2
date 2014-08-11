043132ea53ddeaa14370dde6effbfd7e56b102d7
CoherenceController <qlab@bbn.com>
2014-08-11 14:11:34 -0400

    Fill in MATLAB driver
    --BRJ

--- src/matlab/APS2.m
+++ src/matlab/APS2.m
@@ -1,46 +1,130 @@
+% MATLAB wrapper for the APS2 driver.
+
+% Original author: Blake Johnson
+% Date: August 11, 2014
+
+% Copyright 2014 Raytheon BBN Technologies
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
 classdef APS2 < handle
-   properties
-       serial
-       libpath = 'C:\Users\rsl\Downloads\libaps2-v0.2\build';
-   end
-   
-   methods
-       function obj = APS2()
-           if ~libisloaded('libaps2')
-               loadlibrary(fullfile(obj.libpath, 'libaps2.dll'), fullfile(obj.libpath, 'libaps.h'));
-               calllib('libaps2', 'init');
-           end
-       end
-       
-       function delete(obj)
-           unloadlibrary('libaps2')
-       end
-       
-       function connect(obj, serial)
-           obj.serial = serial;
-           calllib('libaps2', 'connect_APS', serial);
-           calllib('libaps2', 'initAPS', serial, 0);
-       end
-       
-       function disconnect(obj)
-           calllib('libaps2', 'disconnect_APS', obj.serial);
-       end
-       
-       function run(obj)
-           calllib('libaps2', 'run', obj.serial);
-       end
-       
-       function stop(obj)
-           calllib('libaps2', 'stop', obj.serial);
-       end
-       
-       function set_trigger_interval(obj, val)
-           calllib('libaps2', 'set_trigger_interval', obj.serial, val);
-       end
-       
-       function load_sequence(obj, filename)
-           calllib('libaps2', 'load_sequence_file', obj.serial, filename);
-       end
-   end
+    properties
+        serial
+        libpath = 'C:\Users\qlab\Documents\GitHub\libaps2\build';
+    end
+    
+    methods
+        function obj = APS2()
+            if ~libisloaded('libaps2')
+                curPath = fileparts(mfilename('fullpath'));
+                loadlibrary(fullfile(obj.libpath, 'libaps2.dll'), fullfile(curPath, 'libaps.matlab.h'));
+            end
+        end
+        
+        function delete(obj)
+            try
+               obj.disconnect();
+            catch %#ok<CTCH>
+            end
+        end
+        
+        function [serials] = enumerate(obj)
+            calllib('libaps2', 'enumerate_devices');
+            numDevices = calllib('libaps2', 'get_numDevices');
+            serials = cell(1,numDevices);
+            for ct = 1:numDevices
+                serials{ct} = '';
+            end
+            serialPtr = libpointer('stringPtrPtr', serials);
+            serials = calllib('libaps2', 'get_deviceSerials', serialPtr);
+        end
+        
+        function connect(obj, serial)
+            obj.serial = serial;
+            calllib('libaps2', 'connect_APS', serial);
+            calllib('libaps2', 'initAPS', serial, 0);
+        end
+        
+        function disconnect(obj)
+            calllib('libaps2', 'disconnect_APS', obj.serial);
+        end
+        
+        function run(obj)
+            calllib('libaps2', 'run', obj.serial);
+        end
+        
+        function stop(obj)
+            calllib('libaps2', 'stop', obj.serial);
+        end
+        
+        function val = get_running(obj)
+            val = calllib('libaps2', 'get_running', obj.serial);
+        end
+        
+        % trigger methods
+        function val = get_trigger_interval(obj)
+            val = calllib('libaps2', 'get_trigger_interval', obj.serial);
+        end
+        
+        function set_trigger_interval(obj, val)
+            calllib('libaps2', 'set_trigger_interval', obj.serial, val);
+        end
+        
+        function val = get_trigger_source(obj)
+            triggerSourceMap = containers.Map({0, 1}, {'external', 'internal'});
+            val = calllib('libaps2', 'get_trigger_source', obj.serial);
+            val = triggerSourceMap(val);
+        end
+        
+        function set_trigger_source(obj, source)
+            triggerSourceMap = containers.Map({'external', 'ext', 'internal', 'int'}, {0, 0, 1, 1});
+            calllib('libaps2', 'set_trigger_source', obj.serial, triggerSourceMap(lower(source)));
+        end
+        
+        % waveform and instruction data methods
+        function load_sequence(obj, filename)
+            calllib('libaps2', 'load_sequence_file', obj.serial, filename);
+        end
+
+        % channel methods
+        function val = get_channel_offset(obj, channel)
+            val = calllib('libaps2', 'get_channel_offset', obj.serial, channel-1);
+        end
+        
+        function set_channel_offset(obj, channel, offset)
+            calllib('libaps2', 'set_channel_offset', obj.serial, channel-1, offset);
+        end
+
+        function val = get_channel_scale(obj, channel)
+            val = calllib('libaps2', 'get_channel_scale', obj.serial, channel-1);
+        end
+        
+        function set_channel_scale(obj, channel, scale)
+            calllib('libaps2', 'set_channel_scale', obj.serial, channel-1, scale);
+        end
+
+        function val = get_channel_enabled(obj, channel)
+            val = calllib('libaps2', 'get_channel_enabled', obj.serial, channel-1);
+        end
+        
+        function set_channel_enabled(obj, channel, enabled)
+            calllib('libaps2', 'set_channel_enabled', obj.serial, channel-1, enabled);
+        end
+        
+        % debug methods
+        function set_logging_level(obj, level)
+            calllib('libaps2', 'set_logging_level', level);
+        end
+    end
     
 end
---
+++ src/matlab/libaps.matlab.h
@@ -0,0 +1,98 @@
+/*
+ * libaps.h
+ *
+ *  Created on: Jun 25, 2012
+ *      Author: qlab
+ */
+
+#ifndef LIBAPS_H_
+#define LIBAPS_H_
+
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum APSErrorCode {
+	APS_OK,
+	APS_UNKNOWN_ERROR = -1,
+	APS_FILE_ERROR = -2
+};
+
+EXPORT int enumerate_devices();
+EXPORT int get_numDevices();
+EXPORT void get_deviceSerials(const char **);
+
+EXPORT int connect_APS(const char *);
+EXPORT int disconnect_APS(const char *);
+
+EXPORT int reset(const char *, int);
+EXPORT int initAPS(const char *, int);
+EXPORT int get_firmware_version(const char *);
+
+EXPORT double get_uptime(const char *);
+
+EXPORT int set_sampleRate(const char *, int);
+EXPORT int get_sampleRate(const char *);
+
+EXPORT int set_channel_offset(const char *, int, float);
+EXPORT float get_channel_offset(const char *, int);
+EXPORT int set_channel_scale(const char *, int, float);
+EXPORT float get_channel_scale(const char *, int);
+EXPORT int set_channel_enabled(const char *, int, int);
+EXPORT int get_channel_enabled(const char *, int);
+
+EXPORT int set_trigger_source(const char *, int);
+EXPORT int get_trigger_source(const char *);
+EXPORT int set_trigger_interval(const char *, double);
+EXPORT double get_trigger_interval(const char *);
+
+EXPORT int set_waveform_float(const char *, int, float*, int);
+EXPORT int set_waveform_int(const char *, int, short*, int);
+EXPORT int set_markers(const char *, int, char*, int);
+
+EXPORT int write_sequence(const char *, unsigned __int64*, unsigned int);
+
+EXPORT int set_run_mode(const char *, int);
+
+EXPORT int load_sequence_file(const char *, const char*);
+
+EXPORT int clear_channel_data(const char *);
+
+EXPORT int run(const char *);
+EXPORT int stop(const char *);
+
+EXPORT int get_running(const char *);
+
+EXPORT int set_log(const char *);
+EXPORT int set_logging_level(int);
+
+EXPORT const char * get_ip_addr(const char *);
+EXPORT int set_ip_addr(const char *, const char *);
+
+/* private API methods */
+
+EXPORT int write_memory(const char *, unsigned int, unsigned int*, unsigned int);
+EXPORT int read_memory(const char *, unsigned int, unsigned int*, unsigned int);
+EXPORT int read_register(const char *, unsigned int);
+EXPORT int program_FPGA(const char *, const char *);
+
+EXPORT int write_flash(const char *, unsigned int, unsigned int*, unsigned int);
+EXPORT int read_flash(const char *, unsigned int, unsigned int, unsigned int*);
+
+EXPORT unsigned __int64 get_mac_addr(const char *);
+EXPORT int set_mac_addr(const char *, unsigned __int64);
+
+EXPORT int write_SPI_setup(const char *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBAPS_H_ */
