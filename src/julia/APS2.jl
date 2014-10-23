d4751d8d28908c51e16931e781e289fd8ad12af8
Colm Ryan <colm@colmryan.org>
2014-10-23 11:20:44 -0400

    Fancy macros for new APS2_STATUS checking

--- src/julia/APS2.jl
+++ src/julia/APS2.jl
@@ -4,62 +4,117 @@ end
 
 push!(DL_LOAD_PATH, joinpath(dirname(@__FILE__), "../../build/"))
 
+typealias APS2_STATUS Cint
+
+const APS2_OK = 0
+const APS2_ERROR_MSG_LEN = 256
+
+function check_status(status::APS2_STATUS)
+	if status != APS2_OK
+		msg = Array(Uint8, APS2_ERROR_MSG_LEN)
+		ccall((:get_error_msg, "libaps2"), APS2_STATUS, (APS2_STATUS, Ptr{Cchar}), status, msg) 
+		error("APS2 library call failed with error: $(bytestring(msg))")
+	end
+end
+
+macro aps2_getter(funcName, dataType)
+	funcProto = :($funcName(aps::APS2))
+	funcBody = quote
+				val = Array($dataType, 1)
+				status = ccall(($funcName, "libaps2"), APS2_STATUS, (Ptr{Cchar}, Ptr{$dataType}), aps.serial, val)
+				check_status(status)
+				return val[0]
+			end
+	Expr(:function, esc(funcProto), esc(funcBody))
+end
+
+macro aps2_setter(funcName, dataType)
+	funcProto = :($funcName(aps::APS2), val::$dataType)
+	funcBody = quote
+				status = ccall(($funcName, "libaps2"), APS2_STATUS, (Ptr{Cchar}, $dataType), aps.serial, val)
+				check_status(status)
+			end
+	Expr(:function, esc(funcProto), esc(funcBody))
+end
+
+macro aps2_call(funcName, args...)
+	funcProto = :($funcName(aps::APS2))
+	funcBody = quote
+				status = ccall(($funcName, "libaps2"), APS2_STATUS, (Ptr{Cchar},), aps.serial)
+				check_status(status)
+			end
+	for ct in 1:length(args)
+		argName = symbol("arg$ct")
+		push!(funcProto.args, :($argName::$(args[ct]) ) )
+		push!(funcBody.args[2].args[2].args, :($argName))
+	end
+	Expr(:function, esc(funcProto), esc(funcBody))
+end
+	
 function connect!(aps::APS2, serial)
-	ccall((:connect_APS, "libaps2"), Cint, (Ptr{Cchar},), serial)
+	status = ccall((:connect_APS, "libaps2"), APS2_STATUS, (Ptr{Cchar},), serial)
+	check_status(status)
 	aps.serial = serial
 end
 
-disconnect(aps::APS2) = ccall((:disconnect_APS, "libaps2"), Cint, (Ptr{Cchar},), aps.serial)
-
-init(aps::APS2) = ccall((:initAPS, "libaps2"), Cint, (Ptr{Cchar}, Cint), aps.serial, 1)
-
-set_logging_level(level) = ccall(("set_logging_level", "libaps2"), Cint, (Cint,), level)
-
-run(aps::APS2) = ccall((:run, "libaps2"), Cint, (Ptr{Cchar},), aps.serial)
-stop(aps::APS2) = ccall((:stop, "libaps2"), Cint, (Ptr{Cchar},), aps.serial)
-
-function read_memory(aps::APS2, addr, numWords)
-	buf = Array(Uint32, numWords)
-	ccall((:read_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, buf, numWords)
-	buf
+function disconnect!(aps::APS2) 
+	status = ccall((:disconnect_APS, "libaps2"), APS2_STATUS, (Ptr{Cchar},), aps.serial)
+	check_status(status)
+	aps.serial = ""
 end
 
-write_memory(aps::APS2, addr, data::Vector{Uint32}) =
-	ccall((:write_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, data, length(data))
+@aps2_call init Cint
 
-set_channel_offset(aps::APS2, chan, offset) =
-	ccall((:set_channel_offset, "libaps2"), Cint, (Ptr{Cchar}, Cint, Float32), aps.serial, chan, offset)
+@aps2_call set_logging_level Cint
 
-get_firmware_version(aps::APS2) = ccall((:get_firmware_version, "libaps2"), Cint, (Ptr{Cchar},), aps.serial)
+@aps2_call run
+@aps2_call stop
 
-function run_DAC_BIST(aps::APS2, dac, testVec::Vector{Int16})
-	results = Array(Uint32, 8)
-	passed = ccall((:run_DAC_BIST, "libaps2"),
-						Cint, (Ptr{Cchar}, Cint, Ptr{Int16}, Cuint, Ptr{Uint32}),
-						aps.serial, dac, testVec, length(testVec), results)
-	return passed, results
-end
+@aps2_setter set_channel_offset Float32
+@aps2_getter get_channel_offset Float32
 
-function test_BIST_bits(aps::APS2, dac)
+# function read_memory(aps::APS2, addr, numWords)
+# 	buf = Array(Uint32, numWords)
+# 	ccall((:read_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, buf, numWords)
+# 	buf
+# end
 
-	passedVec = Array(Int, 14)
-	for bit = 0:13
-		print("Testing bit $bit: ")
-		testVec = (int16(rand(Bool, 100)) .<< bit) * (bit == 13 ? -1 : 1)
-		passedVec[bit+1], bistVals = run_DAC_BIST(aps, dac, testVec)
-		FPGAPhase1 = bistVals[3] == bistVals[1] ? "pass" : "fail"
-		FPGAPhase2 = bistVals[4] == bistVals[2] ? "pass" : "fail"
-		LVDSPhase1 = bistVals[5] == bistVals[1] ? "pass" : "fail"
-		LVDSPhase2 = bistVals[6] == bistVals[2] ? "pass" : "fail"
-		SYNCPhase1 = bistVals[7] == bistVals[7] ? "pass" : "fail"
-		SYNCPhase2 = bistVals[8] == bistVals[8] ? "pass" : "fail"
-		println("FPGA: $FPGAPhase1 / $FPGAPhase2; LVDS: $LVDSPhase1 / $LVDSPhase2; SYNC: $SYNCPhase1 / $SYNCPhase2")
-	end
+# write_memory(aps::APS2, addr, data::Vector{Uint32}) =
+# 	ccall((:write_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, data, length(data))
 
-	return passedVec
-end
+# set_channel_offset(aps::APS2, chan, offset) =
+# 	ccall((:set_channel_offset, "libaps2"), Cint, (Ptr{Cchar}, Cint, Float32), aps.serial, chan, offset)
 
-set_DAC_SD(aps::APS2, dac, sd) = ccall((:set_DAC_SD, "libaps2"), Cint, (Ptr{Cchar}, Cint, Cchar), aps.serial, dac, sd)
+# get_firmware_version(aps::APS2) = library_call(aps, :get_firmware_version, Uint32)
+
+# function run_DAC_BIST(aps::APS2, dac, testVec::Vector{Int16})
+# 	results = Array(Uint32, 8)
+# 	passed = ccall((:run_DAC_BIST, "libaps2"),
+# 						Cint, (Ptr{Cchar}, Cint, Ptr{Int16}, Cuint, Ptr{Uint32}),
+# 						aps.serial, dac, testVec, length(testVec), results)
+# 	return passed, results
+# end
+
+# function test_BIST_bits(aps::APS2, dac)
+
+# 	passedVec = Array(Int, 14)
+# 	for bit = 0:13
+# 		print("Testing bit $bit: ")
+# 		testVec = (int16(rand(Bool, 100)) .<< bit) * (bit == 13 ? -1 : 1)
+# 		passedVec[bit+1], bistVals = run_DAC_BIST(aps, dac, testVec)
+# 		FPGAPhase1 = bistVals[3] == bistVals[1] ? "pass" : "fail"
+# 		FPGAPhase2 = bistVals[4] == bistVals[2] ? "pass" : "fail"
+# 		LVDSPhase1 = bistVals[5] == bistVals[1] ? "pass" : "fail"
+# 		LVDSPhase2 = bistVals[6] == bistVals[2] ? "pass" : "fail"
+# 		SYNCPhase1 = bistVals[7] == bistVals[7] ? "pass" : "fail"
+# 		SYNCPhase2 = bistVals[8] == bistVals[8] ? "pass" : "fail"
+# 		println("FPGA: $FPGAPhase1 / $FPGAPhase2; LVDS: $LVDSPhase1 / $LVDSPhase2; SYNC: $SYNCPhase1 / $SYNCPhase2")
+# 	end
+
+# 	return passedVec
+# end
+
+# set_DAC_SD(aps::APS2, dac, sd) = ccall((:set_DAC_SD, "libaps2"), Cint, (Ptr{Cchar}, Cint, Cchar), aps.serial, dac, sd)
 
 function create_test_waveform()
 	#Create a test pattern with the follow pattens separated by 10ns of zero:
