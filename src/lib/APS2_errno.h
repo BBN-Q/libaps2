469f62f0b654f33957eb04e74da0068351a67369
Colm Ryan <colm@colmryan.org>
2014-10-23 15:32:29 -0400

    errno header file

---
+++ src/lib/APS2_errno.h
@@ -0,0 +1,23 @@
+#ifndef APS2_ERRNO_H_
+#define APS2_ERRNO_H_
+
+#include <map>
+#include <string>
+
+enum APS2_STATUS {
+	APS2_OK,
+	APS2_UNKNOWN_ERROR = -1,
+	APS2_FILE_ERROR = -2,
+	APS2_UNCONNECTED = -3,
+	APS2_NO_DEVICE_FOUND = -4,
+	APS2_RESET_TIMEOUT =-5,
+};
+
+static std::map<APS2_STATUS, std::string> messages = {
+	{APS2_UNCONNECTED, "Attempt to run library function on unconnected APS2"}
+};
+
+
+
+
+#endif
