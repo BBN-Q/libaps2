8eb1ca304ca30103240dc6b959ac4d36906971f1
Colm Ryan <colm@colmryan.org>
2014-10-23 20:04:20 -0400

    Bump MAX_WF_LENGTH to 128kS

--- src/lib/constants.h
+++ src/lib/constants.h
@@ -17,7 +17,7 @@ static const int  MAX_APS_CHANNELS = 2;
 
 static const int  APS_WAVEFORM_UNIT_LENGTH = 4;
 
-static const int MAX_WF_LENGTH = 65536;
+static const int MAX_WF_LENGTH = 131072;
 static const int MAX_WF_AMP = 8191;
 static const int WF_MODULUS = 4;
 static const size_t MAX_LL_LENGTH = (1 << 24);
