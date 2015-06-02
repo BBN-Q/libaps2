6f766c8c83d9fc15bc5f7d37718e8b00ae6a0f0c
Colm Ryan <cryan@bbn.com>
2015-06-01 20:44:25 -0400

    Include stdint for fixed with integer support in C library
    Should support Matlab on either Windows or Linux.

--- src/matlab/libaps2.matlab.h
+++ src/matlab/libaps2.matlab.h
@@ -14,17 +14,15 @@ enum TLogLevel {logERROR, logWARNING, logINFO, logDEBUG, logDEBUG1, logDEBUG2, l
 
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)
-#define UINT64 unsigned __int64
 #else
-#include <stdint.h>
 #define EXPORT
-#define UINT64 uint64_t
 #endif
-
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <stdint.h> //fixed-width integer types
 
 //Typedef the enums for C compatibility
 typedef enum APS2_STATUS APS2_STATUS;
@@ -68,7 +66,7 @@ EXPORT APS2_STATUS set_waveform_float(const char*, int, float*, int);
 EXPORT APS2_STATUS set_waveform_int(const char*, int, short*, int);
 EXPORT APS2_STATUS set_markers(const char*, int, char*, int);
 
-EXPORT APS2_STATUS write_sequence(const char*, UINT64*, unsigned int);
+EXPORT APS2_STATUS write_sequence(const char*, uint64_t*, unsigned int);
 
 EXPORT APS2_STATUS set_run_mode(const char*, RUN_MODE);
 
@@ -96,8 +94,8 @@ EXPORT int program_FPGA(const char*, const char*);
 EXPORT int write_flash(const char*, unsigned int, unsigned int*, unsigned int);
 EXPORT int read_flash(const char*, unsigned int, unsigned int, unsigned int*);
 
-EXPORT UINT64 get_mac_addr(const char*);
-EXPORT int set_mac_addr(const char*, UINT64);
+EXPORT uint64_t get_mac_addr(const char*);
+EXPORT int set_mac_addr(const char*, uint64_t);
 
 EXPORT APS2_STATUS write_SPI_setup(const char*);
 
