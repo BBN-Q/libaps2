43ce00bbd0708c3572fcf9611f5b03042e160843
Colm Ryan <cryan@bbn.com>
2016-01-22 16:14:58 -0500

    :white_check_mark: command line argument to choose test device

--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -1,2 +1,28 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#include <iostream>
+#include <string>
+using std::string;
+
+string ip_addr;
+
+int main( int argc, char* const argv[] )
+{
+
+  //Pull out IP address from first argument
+  if ( argc < 2) {
+    std::cerr << "Error! Need to specifiy an IP address to test." << std::endl;
+    return -1;
+  } else {
+    ip_addr = string(argv[1]);
+    memset(argv[1], 0, strlen(argv[1]));
+  }
+
+  // Pass onto Catch
+  int result = Catch::Session().run( argc, argv );
+
+  // global clean-up...
+
+  return result;
+}
