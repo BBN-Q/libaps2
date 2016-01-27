6ee2128e3c2d3625e796195b64e6105e0b663690
Colm Ryan <cryan@bbn.com>
2016-01-27 16:46:15 -0500

    :art: smaller functions

--- test/test_comms.cpp
+++ test/test_comms.cpp
@@ -14,6 +14,8 @@ using std::endl;
 
 extern string ip_addr; //ip address from run_tests
 
+static std::default_random_engine generator(std::random_device{}());
+
 // N-wide hex output with 0x
 template <unsigned int N>
 std::ostream& hexn(std::ostream& out)
@@ -21,47 +23,55 @@ std::ostream& hexn(std::ostream& out)
   return out << "0x" << std::hex << std::setw(N) << std::setfill('0');
 }
 
+vector<uint32_t> random_data(size_t length) {
+  //Create random data to write/read
+	std::uniform_int_distribution<uint32_t> word_distribution;
+	vector<uint32_t> data(length);
+	for (auto & val : data){
+    val = word_distribution(generator);
+	}
+  return data;
+}
+
+uint32_t random_address(uint32_t mem_low, uint32_t mem_high, uint32_t alignment) {
+  //Create a random starting address aligned appropriately
+  //Choose a random starting point
+	std::uniform_int_distribution<uint32_t> addr_distribution(mem_low, mem_high);
+	uint32_t start_addr = addr_distribution(generator);
+	start_addr &= ~(alignment); //align address
+  return start_addr;
+}
+
 bool test_mem_write_read(uint32_t memStartAddr, uint32_t memHighAddr, size_t testLength, uint32_t alignmentMask, bool printSpeed){
 	/*
 	Helper function to test writing and reading to a range of memory with a particular alignment.
 	*/
-	//Generate a random vector to write
-	std::default_random_engine generator;
-	std::uniform_int_distribution<uint32_t> wordDistribution;
 
-	vector<uint32_t> testVec(testLength/4);
-	for (size_t ct=0; ct < (testLength/4); ct++){
-		testVec[ct] = wordDistribution(generator);
-	}
-
-	//Choose a random starting point
-	std::uniform_int_distribution<uint32_t> addrDistribution(memStartAddr, memHighAddr-testLength);
-	uint32_t testStartAddr = addrDistribution(generator);
-	testStartAddr &= ~(alignmentMask); //align address
+  auto test_vec = random_data(testLength/4);
+  auto start_addr = random_address(memStartAddr, memHighAddr-testLength, alignmentMask);
 
   if ( printSpeed ) {
-    cout << "writing " << std::dec << testLength/(1 << 10) << " kB starting at " << hexn<8> << testStartAddr << "..... ";
+    cout << "writing " << std::dec << testLength/(1 << 10) << " kB starting at " << hexn<8> << start_addr << "..... ";
     cout.flush();
     auto start = std::chrono::steady_clock::now();
-    write_memory(ip_addr.c_str(), testStartAddr, testVec.data(), testVec.size());
+    write_memory(ip_addr.c_str(), start_addr, test_vec.data(), test_vec.size());
     auto stop = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop-start).count();
     cout << "at " << static_cast<double>(testLength)/duration << " MB/s." << concol::RESET << endl;
   } else {
-    write_memory(ip_addr.c_str(), testStartAddr, testVec.data(), testVec.size());
+    write_memory(ip_addr.c_str(), start_addr, test_vec.data(), test_vec.size());
   }
 
-
 	//Check a few 1kB count entries for correctness
-	addrDistribution = std::uniform_int_distribution<uint32_t>(testStartAddr, testStartAddr+testLength-1024);
+	std::uniform_int_distribution<uint32_t> addr_distribution(start_addr, start_addr+testLength-1024);
   bool passed = true;
 	for (size_t ct=0; ct < 10; ct++){
 		vector<uint32_t> checkVec(256, 0);
-		uint32_t checkAddr = addrDistribution(generator);
+		uint32_t checkAddr = addr_distribution(generator);
 		checkAddr &= ~(alignmentMask);
 		read_memory(ip_addr.c_str(), checkAddr, checkVec.data(), 256);
 		for (auto val : checkVec){
-			if (val != testVec[(checkAddr-testStartAddr)/4]) {
+			if (val != test_vec[(checkAddr-start_addr)/4]) {
 				passed &= false;
 			}
 			checkAddr += 4;
@@ -154,29 +164,52 @@ TEST_CASE("memory writing and reading", "[read_memory,write_memory]") {
   SECTION("CACHE BRAMs write/read") {
     bool passed;
     //sequence
-    passed = test_mem_write_read(0xc2000000, 0xc2007fff, 1 << 12, 0x3, false);
+    cout << "seq. cache: ";
+    passed = test_mem_write_read(0xc2000000, 0xc2003fff, 1 << 12, 0x3, true);
     REQUIRE(passed);
     // wfA
-    passed = test_mem_write_read(0xc4000000, 0xc403ffff, 1 << 14, 0x3, false);
+    cout << "wf. a cache: ";
+    passed = test_mem_write_read(0xc4000000, 0xc403ffff, 1 << 14, 0x3, true);
     REQUIRE(passed);
     //wfB
-    passed = test_mem_write_read(0xc6000000, 0xc603ffff, 1 << 14, 0x3, false);
+    cout << "wf. b cache: ";
+    passed = test_mem_write_read(0xc6000000, 0xc603ffff, 1 << 14, 0x3, true);
     REQUIRE(passed);
   }
 
-  SECTION("SDRAM write/read") {
+  SECTION("sequencer SDRAM write/read") {
     //Older firmware is too slow for long write tests
     uint32_t firmware_version;
     get_firmware_version(ip_addr.c_str(), &firmware_version);
     size_t testLength = ( (firmware_version & 0x00000fff) >= 0x300) ? (1 << 25) : (1 << 21);
     bool passed;
     //sequence
+    cout << "seq. memory: ";
     passed = test_mem_write_read(SEQ_OFFSET, SEQ_OFFSET + (1 << 29), testLength, 0xf, true);
     REQUIRE(passed);
     //waveform
+    cout << "wf. memory: ";
     passed = test_mem_write_read(WFA_OFFSET, WFA_OFFSET + (1 << 29), testLength, 0xf, true);
     REQUIRE(passed);
   }
+
+  // SECTION("bitfile configuration SDRAM write/read") {
+  //   //Older firmware is too slow for long write tests
+  //   uint32_t firmware_version;
+  //   get_firmware_version(ip_addr.c_str(), &firmware_version);
+  //   size_t testLength = ( (firmware_version & 0x00000fff) >= 0x300) ? (1 << 22) : (1 << 20);
+  //   testLength = 32*4;
+  //   //write to configuration memory
+  //
+  //
+  //   bool passed;
+  //   //sequence
+  //   passed = test_mem_write_read(SEQ_OFFSET, SEQ_OFFSET + (1 << 29), testLength, 0xf, true);
+  //   REQUIRE(passed);
+  //   //waveform
+  //   passed = test_mem_write_read(WFA_OFFSET, WFA_OFFSET + (1 << 29), testLength, 0xf, true);
+  //   REQUIRE(passed);
+  // }
 
 	disconnect_APS(ip_addr.c_str());
 
