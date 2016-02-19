73c76412fc239e8b3530a3df5f4b9dea174dc8b5
Colm Ryan <cryan@bbn.com>
2016-02-19 14:40:13 -0500

    Uint deprecated in Julia

--- src/julia/LIBAPS2.jl
+++ src/julia/LIBAPS2.jl
@@ -40,9 +40,9 @@ typealias APS2_STATUS Cint
 
 const APS2_OK = 0
 
-const EXTERNAL = 0 
+const EXTERNAL = 0
 const INTERNAL = 1
-const SOFTWARE = 2 
+const SOFTWARE = 2
 
 const RUN_SEQUENCE = 0
 const TRIG_WAVEFORM = 1
@@ -50,7 +50,7 @@ const CW_WAVEFORM = 2
 
 function check_status(status::APS2_STATUS)
 	if status != APS2_OK
-		msg = ccall((:get_error_msg, "libaps2"), Ptr{Uint8}, (APS2_STATUS,), status) 
+		msg = ccall((:get_error_msg, "libaps2"), Ptr{UInt8}, (APS2_STATUS,), status)
 		error("APS2 library call failed with error: $(bytestring(msg))")
 	end
 end
@@ -60,7 +60,7 @@ macro aps2_getter(funcName, dataType)
 	funcNameBis = string(funcName)
 	funcBody = quote
 				val = Array($dataType, 1)
-				status = ccall(($funcNameBis, "libaps2"), APS2_STATUS, (Ptr{Uint8}, Ptr{$dataType}), aps.serial, val)
+				status = ccall(($funcNameBis, "libaps2"), APS2_STATUS, (Ptr{UInt8}, Ptr{$dataType}), aps.serial, val)
 				check_status(status)
 				return val[1]
 			end
@@ -71,7 +71,7 @@ macro aps2_setter(funcName, dataType)
 	funcProto = :($funcName(aps::APS2, val::$dataType))
 	funcNameBis = string(funcName)
 	funcBody = quote
-				status = ccall(($funcNameBis, "libaps2"), APS2_STATUS, (Ptr{Uint8}, $dataType), aps.serial, val)
+				status = ccall(($funcNameBis, "libaps2"), APS2_STATUS, (Ptr{UInt8}, $dataType), aps.serial, val)
 				check_status(status)
 			end
 	Expr(:function, esc(funcProto), esc(funcBody))
@@ -85,7 +85,7 @@ macro aps2_call(funcName, args...)
 	end
 	funcNameBis = string(funcName)
 	funcBody = quote
-				status = ccall(($funcNameBis, "libaps2"), APS2_STATUS, (Ptr{Uint8},), aps.serial, $(argNames...))
+				status = ccall(($funcNameBis, "libaps2"), APS2_STATUS, (Ptr{UInt8},), aps.serial, $(argNames...))
 				check_status(status)
 			end
 	Expr(:function, esc(funcProto), esc(funcBody))
@@ -99,35 +99,35 @@ end
 
 function enumerate_APS2s()
 	numDevices = get_numDevices()
-	Cserials = Array(Ptr{Uint8}, numDevices)
-	status = ccall((:get_deviceSerials, "libaps2"), APS2_STATUS, (Ptr{Ptr{Uint8}},), Cserials)
+	Cserials = Array(Ptr{UInt8}, numDevices)
+	status = ccall((:get_deviceSerials, "libaps2"), APS2_STATUS, (Ptr{Ptr{UInt8}},), Cserials)
 	serials = Array(ASCIIString, 0)
 	for ct = 1:numDevices
 		push!(serials, bytestring(Cserials[ct]))
 	end
 	return (numDevices, serials)
 end
-	
+
 function connect!(aps::APS2, serial)
-	status = ccall((:connect_APS, "libaps2"), APS2_STATUS, (Ptr{Uint8},), serial)
+	status = ccall((:connect_APS, "libaps2"), APS2_STATUS, (Ptr{UInt8},), serial)
 	check_status(status)
 	aps.serial = serial
 end
 
-function disconnect!(aps::APS2) 
-	status = ccall((:disconnect_APS, "libaps2"), APS2_STATUS, (Ptr{Uint8},), aps.serial)
+function disconnect!(aps::APS2)
+	status = ccall((:disconnect_APS, "libaps2"), APS2_STATUS, (Ptr{UInt8},), aps.serial)
 	check_status(status)
 	aps.serial = ""
 end
 
 function init(aps::APS2, force=1)
-	status = ccall((:init_APS, "libaps2"), APS2_STATUS, (Ptr{Uint8}, Cint), aps.serial, Cint(force))
+	status = ccall((:init_APS, "libaps2"), APS2_STATUS, (Ptr{UInt8}, Cint), aps.serial, Cint(force))
 	check_status(status)
 end
 
 set_logging_level(level) = ccall((:set_logging_level, "libaps2"), APS2_STATUS, (Cint,), level)
 
-@aps2_getter get_firmware_version Uint32
+@aps2_getter get_firmware_version UInt32
 @aps2_getter get_uptime Float64
 @aps2_getter get_fpga_temperature Float64
 
@@ -152,28 +152,28 @@ set_logging_level(level) = ccall((:set_logging_level, "libaps2"), APS2_STATUS, (
 @aps2_call trigger
 
 function load_waveform{T<:Integer}(aps::APS2, chan, wf::Vector{T})
-	status = ccall((:set_waveform_int, "libaps2"), APS2_STATUS, (Ptr{Uint8}, Cint, Ptr{Int16}, Uint32), aps.serial, Cint(chan-1), int16(wf), length(wf))
+	status = ccall((:set_waveform_int, "libaps2"), APS2_STATUS, (Ptr{UInt8}, Cint, Ptr{Int16}, UInt32), aps.serial, Cint(chan-1), int16(wf), length(wf))
 	check_status(status)
 end
 
 function load_waveform{T<:Real}(aps::APS2, chan, wf::Vector{T})
-	status = ccall((:set_waveform_float, "libaps2"), APS2_STATUS, (Ptr{Uint8}, Cint, Ptr{Float32}, Uint32), aps.serial, Cint(chan-1), float32(wf), length(wf))
+	status = ccall((:set_waveform_float, "libaps2"), APS2_STATUS, (Ptr{UInt8}, Cint, Ptr{Float32}, UInt32), aps.serial, Cint(chan-1), float32(wf), length(wf))
 	check_status(status)
 end
 
 function load_sequence(aps::APS2, seqfile)
-	status = ccall((:load_sequence_file, "libaps2"), APS2_STATUS, (Ptr{Uint8}, Ptr{Uint8}), aps.serial, seqfile)
+	status = ccall((:load_sequence_file, "libaps2"), APS2_STATUS, (Ptr{UInt8}, Ptr{UInt8}), aps.serial, seqfile)
 	check_status(status)
 end
 
 function read_memory(aps::APS2, addr, numWords)
-	buf = Array(Uint32, numWords)
-	ccall((:read_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, buf, numWords)
+	buf = Array(UInt32, numWords)
+	ccall((:read_memory, "libaps2"), Cint, (Ptr{Cchar}, UInt32, Ptr{UInt32}, UInt32), aps.serial, addr, buf, numWords)
 	buf
 end
 
-write_memory(aps::APS2, addr, data::Vector{Uint32}) =
-	ccall((:write_memory, "libaps2"), Cint, (Ptr{Cchar}, Uint32, Ptr{Uint32}, Uint32), aps.serial, addr, data, length(data))
+write_memory(aps::APS2, addr, data::Vector{UInt32}) =
+	ccall((:write_memory, "libaps2"), Cint, (Ptr{Cchar}, UInt32, Ptr{UInt32}, UInt32), aps.serial, addr, data, length(data))
 
 
 function create_test_waveform()
