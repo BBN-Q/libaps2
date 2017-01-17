ee019cb80dd1deb7648eb33df00e69e13e336afe
Blake R. Johnson <blake.johnson@raytheon.com>
2017-01-17 16:56:23 -0500

    Also need to bump version in setup.py
    Really need to figure out how to automate this...

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libaps2',
-    version="1.2",
+    version="1.3",
     url='https://github.com/BBN-Q/libaps2',
     py_modules=["aps2"]
 )
