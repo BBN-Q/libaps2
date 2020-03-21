9aacf42ba60372c6db1116089fd8ceb9005b7d0d
Matthew Ware <matthewware@users.noreply.github.com>
2020-03-21 16:00:28 -0400

    Update setup.py
    numpy is a dep after all.

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,5 +4,8 @@ setup(
     name='libaps2',
     version="1.3",
     url='https://github.com/BBN-Q/libaps2',
-    py_modules=["aps2"]
+    py_modules=["aps2"],
+    install_requires=[
+        'numpy>=1.18'
+    ]
 )
